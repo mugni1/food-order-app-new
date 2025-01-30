1ab7a53f83782ac225f26e994bb5a99457f5f7e9
mugni1 <asepabdulmugni22@gmail.com>
2025-01-30 19:52:56 +0700

    modified services

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,14 +1,3 @@
-// export async function getAllItems() {
-//   try {
-//     const res = await fetch("http://localhost:8000/api/items", {
-//       method: "GET",
-//     });
-//     return res.json();
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// }
-
 export async function getWithCategory(name) {
   try {
     let id = 0;
@@ -26,6 +15,9 @@ export async function getWithCategory(name) {
     }
     const res = await fetch("http://localhost:8000/api/items?category=" + id, {
       method: "GET",
+      next: {
+        revalidate: 60 * 60,
+      },
     });
     return res.json();
   } catch (error) {
